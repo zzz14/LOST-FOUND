7c3cb2c967a3e1d0a69d7b239040378206b1bcbc
DESKTOP-GKG54A0\Pi <yepei14@outlook.com>
2016-11-27 15:29:59 +0800

    初始版本，创建了新的数据库

---
+++ lost_and_found/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+
+from codex.baseerror import LogicError
+
+class Lost(models.Model):
+    name = models.CharField(max_length=128)
+    key = models.CharField(max_length=64, db_index=True)
+    contact = models.CharField(max_length=128)
+    description = models.TextField()
+    lostTime = models.DateTimeField(db_index=True)
+    lostPlace = models.CharField(max_length=128)
+    reward = models.CharField(max_length=128)
+
+class Found(models.Model):
+    name = models.CharField(max_length=128)
+    key = models.CharField(max_length=64, db_index=True)
+    contact = models.CharField(max_length=128)
+    description = models.TextField()
+    lostTime = models.DateTimeField(db_index=True)
+    lostPlace = models.CharField(max_length=128)
+    reward = models.CharField(max_length=128)
+
+'''
+class User(models.Model):
+    open_id = models.CharField(max_length=64, unique=True, db_index=True)
+    student_id = models.CharField(max_length=32, unique=True, db_index=True)
+
+    @classmethod
+    def get_by_openid(cls, openid):
+        try:
+            return cls.objects.get(open_id=openid)
+        except cls.DoesNotExist:
+            raise LogicError('User not found')
+
+
+class Activity(models.Model):
+    name = models.CharField(max_length=128)
+    key = models.CharField(max_length=64, db_index=True)
+    description = models.TextField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    place = models.CharField(max_length=256)
+    book_start = models.DateTimeField(db_index=True)
+    book_end = models.DateTimeField(db_index=True)
+    total_tickets = models.IntegerField()
+    status = models.IntegerField()
+    pic_url = models.CharField(max_length=256)
+    remain_tickets = models.IntegerField()
+
+    STATUS_DELETED = -1
+    STATUS_SAVED = 0
+    STATUS_PUBLISHED = 1
+
+
+class Ticket(models.Model):
+    student_id = models.CharField(max_length=32, db_index=True)
+    unique_id = models.CharField(max_length=64, db_index=True, unique=True)
+    activity = models.ForeignKey(Activity)
+    status = models.IntegerField()
+
+    STATUS_CANCELLED = 0
+    STATUS_VALID = 1
+    STATUS_USED = 2
+'''
